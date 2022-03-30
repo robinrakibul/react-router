b19cde56dfeddfc86f436a2ad9a7d9a1202154bf
robinrakibul <robinrakibulhassan@gmail.com>
2022-03-30 22:44:54 +0600

    added button dynamically for each users

--- src/components/Friends/Friends.js
+++ src/components/Friends/Friends.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Friend from '../Friend/Friend';
 
 const Friends = () => {
+    const [friends,setFriends] = useState([]);
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=> setFriends(data));
+    },[])
     return (
         <div>
-            <h2>Friends</h2>
+            <h2>Total Friends: {friends.length}</h2>
+            {
+                friends.map(friend=> <Friend key={friends.id} friend={friend} id={friend.id}></Friend>)
+            }
         </div>
     );
 };
